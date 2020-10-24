ceaf49f6e18aa947faba0eed7ac94d300c318900
Nelson Jawuer Morales Santa <guitarro92@hotmail.com>
2020-10-24 11:50:11 -0500

    EnProducción

--- numero_abandono.php
+++ numero_abandono.php
@@ -2,14 +2,14 @@
     $fech = date("Y-m-d");
 
     include "conexion.php";
-    $anteriores = fopen("anteriores.txt", "a+"); // Oni kreas dosieron .txt
-    $filesize = filesize("anteriores.txt"); // Oni elektas dosieron
+    $anteriores = fopen("anteriores.txt", "a+");
+    $filesize = filesize("anteriores.txt");
     $lista = $filesize ? fread($anteriores,filesize("anteriores.txt")) : "'0',";
 
     $result = pg_query($conexion, "SELECT * FROM vista_abandono_inbound WHERE cola='3014' AND fecha='$fech' AND callid NOT IN (" . trim($lista, ",") . ") ORDER BY hora ASC LIMIT 1");
     while ($consulta = pg_fetch_row($result)) {
         echo $consulta[4];
-        fwrite($anteriores, "'" . $consulta[2] . "',"); // Oni skrivas enne la dosiero .txt
+        fwrite($anteriores, "'" . $consulta[2] . "',");
     }
     include "cerrar.php";
 
